f4581f1f7740b0f9d8d794132a6f8f7588517765
JiaxinD <jiaxindeng0@gmail.com>
2019-08-12 19:04:40 -0700

    Done

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Jiaxin on 8/8/19.
+//  Copyright © 2019 Jiaxin. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+        @IBAction func Close() {
+            dismiss(animated: true, completion: nil)
+        }
+    
+}
+
+
